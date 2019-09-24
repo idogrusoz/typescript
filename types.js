244eda191e2d3b73d5b041d7f2a4f3dec55cbd24
IBRAHIM DOGRUSOZ <idogrusoz@gmail.com>
2019-09-25 00:22:27 +0200

    Add files via upload
    Typescript types and variables

---
+++ types.js
@@ -0,0 +1,29 @@
+var myString;
+var myNum;
+var myBool;
+var myVar;
+var strArr;
+var numArr;
+var boolArr;
+myString = 'Hello World';
+myNum = 22;
+myBool = true;
+myVar = 5 + '5';
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3];
+boolArr = [true, true, false];
+console.log(myString);
+console.log(myNum);
+// Another way of defining variable types
+var newStrArr;
+var newNumArr;
+var newBoolArr;
+var strNumTuple;
+// It must match the above combination. Try to change and see that it gives an error
+// After passing initial pattern it can continue in any way (can be followed by any value)
+strNumTuple = ['Hello', 4];
+var myVoid = null; //can be undefined too
+var myNull = null;
+var myUndefined = undefined;
+var myUndefined2 = undefined;
+var myNull2 = null;
---
+++ types.ts
@@ -0,0 +1,38 @@
+let myString: string
+let myNum: number
+let myBool: boolean
+let myVar: any
+
+let strArr: string[]
+let numArr: number[]
+let boolArr: boolean[]
+
+myString = 'Hello World'
+myNum = 22
+myBool = true
+myVar = 5 + '5'
+
+strArr = ['Hello', 'World']
+numArr = [1, 2, 3]
+boolArr = [true, true, false]
+
+console.log(myString)
+console.log(myNum)
+
+// Another way of defining variable types
+
+let newStrArr: Array<string>
+let newNumArr: Array<number>
+let newBoolArr: Array<boolean>
+
+let strNumTuple: [string, number]
+// It must match the above combination. Try to change and see that it gives an error
+// After passing initial pattern it can continue in any way (can be followed by any value)
+strNumTuple = ['Hello', 4]
+
+let myVoid: void = null //can be undefined too
+
+let myNull: null = null
+let myUndefined: undefined = undefined
+let myUndefined2: null = undefined
+let myNull2: undefined = null
