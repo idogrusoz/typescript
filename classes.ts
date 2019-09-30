9a6df26922c6346ff38a3d1e28a430400f959941
Ibrahim Dogrusoz <idogrusoz@gmail.com>
2019-09-30 16:44:38 +0200

    Class inheritence

--- classes.ts
+++ classes.ts
@@ -15,6 +15,10 @@ class User {
   register() {
     console.log(this.name + ' is now registered')
   }
+
+  payInvoice() {
+    console.log(this.name + ' paid invoice')
+  }
 }
 
 let john = new User('John Doe', 'johndoe@gmail.com', 40)
@@ -22,3 +26,20 @@ let john = new User('John Doe', 'johndoe@gmail.com', 40)
 console.log(john.age)
 
 john.register()
+
+class Member extends User {
+  id: number
+
+  constructor(id: number, name: string, email: string, age: number) {
+    super(name, email, age)
+    this.id = id
+  }
+
+  payInvoice() {
+    super.payInvoice()
+  }
+}
+
+let mike: User = new Member(1, 'Mike Smith', 'mikesmith@gmail.com', 33)
+
+mike.payInvoice()
